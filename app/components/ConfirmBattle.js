ba6ca4a915f46800af3830bdda4ab3bdf9ac7a45
Bruno Rios <brunorios1@gmail.com>
2017-02-03 11:29:22 -0200

    video 8

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -13,12 +16,39 @@ function ConfirmBattle (props) {
     )
   } else {
     return (
-      <div>
-      CONFIRM BATTLE!
-        {puke(props)}
+      <div className="jumbotron col-sm-12 text-center">
+        <h1>Confirm Players</h1>
+        <div className="row">
+          <div className="col-sm-6">
+            Player 1
+            <UserDetails info={props.playersInfo[0]} />
+          </div>
+          <div className="col-sm-6">
+            Player 2
+            <UserDetails info={props.playersInfo[1]} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <div>
+              <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>Initiate Battle</button>
+            </div>
+            <div>
+              <Link to='/playerOne'>
+                <button type="button" className="btn btn-lg btn-danger">Reselect Players</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
